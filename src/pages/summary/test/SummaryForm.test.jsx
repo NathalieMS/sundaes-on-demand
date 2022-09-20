81b55d14e50357a635229c71614e1d3ae1fa2373
NathalieMS <nathalie.moreira@hotmail.com>
2022-09-20 17:23:21 -0300

    Update Teste

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,34 +1,28 @@
 import {
-    render,
-    screen,
-    waitForElementToBeRemoved,
-  } from '@testing-library/react';
-  import SummaryForm from '../SummaryForm';
-  import userEvent from '@testing-library/user-event';
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
+
+test('Initial conditions', () => {
+  const teste = render(<SummaryForm />);
+  teste.getByRole
   
-  test('Initial conditions', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-      name: /terms and conditions/i,
-    });
-    expect(checkbox).not.toBeChecked();
-  
-    const confirmButton = screen.getByRole('button', { name: /confirm order/i });
-    expect(confirmButton).toBeDisabled();
+  const checkbox = teste.getByRole('checkbox', {
+    name: /terms and conditions/i,
   });
-  
-  test('Checkbox enables button on first click and disables on second click', () => {
-    render(<SummaryForm />);
-    const checkbox = screen.getByRole('checkbox', {
-      name: /terms and conditions/i,
-    });
-    const confirmButton = screen.getByRole('button', { name: /confirm order/i });
-  
-    userEvent.click(checkbox);
-    expect(confirmButton).toBeEnabled();
-  
-    userEvent.click(checkbox);
-    expect(confirmButton).toBeDisabled();
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test('Checkbox enables button on first click and disables on second click', () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
   });
   const confirmButton = screen.getByRole('button', { name: /confirm order/i });
 
